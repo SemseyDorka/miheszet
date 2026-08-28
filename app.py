0d8fcaf1647a521e927e6ac5d8483cd32371998f
SemseyDorka <37298534+SemseyDorka@users.noreply.github.com>
2026-08-28 11:00:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.sidebar:
   
     model_option = st.selectbox(
         "Válassz modellt:",
-        [ "llama-3.1-8b-instant"]
+        [ "llama-3.1-8b-instant", "llama-3.3-70b-versatile"]
     )
     
     
