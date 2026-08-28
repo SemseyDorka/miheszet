4d89f8435a8e0ee21202de48443f1c2ad75e63c0
SemseyDorka <37298534+SemseyDorka@users.noreply.github.com>
2026-08-28 11:03:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.sidebar:
   
     model_option = st.selectbox(
         "Válassz modellt:",
-        [ "llama-3.1-8b-instant", "llama-3.3-70b-versatile"]
+        [ "llama-3.1-8b-instant", "llama-3.3-70b-versatile", "openai/gpt-oss-120b"]
     )
     
     
